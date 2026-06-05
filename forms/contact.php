1ea3dd0d2d6c5a9bd5b889e348d3ac559923310d
Disha <d4dishamani.das@gmail.com>
2026-06-05 09:59:45 +0530

    mailer and contact form

--- forms/contact.php
+++ forms/contact.php
@@ -1,42 +1,64 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+require '../phpmailer/src/Exception.php';
+require '../phpmailer/src/PHPMailer.php';
+require '../phpmailer/src/SMTP.php';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  isset($_POST['phone']) && $contact->add_message($_POST['phone'], 'Phone');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    $mail = new PHPMailer(true);
 
-  echo $contact->send();
+    try {
+
+        $mail->isSMTP();
+
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+
+        $mail->Username = 'das.dishamani.work@gmail.com';
+        $mail->Password = 'orsq hvld nydj zmow';
+
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        $mail->setFrom('yourgmail@gmail.com', 'Website Contact Form');
+
+        $mail->addAddress('das.dishamani.work@gmail.com');
+
+        $mail->isHTML(true);
+
+        $mail->Subject = $subject;
+
+        $mail->Body =
+        "
+        <h3>New Contact Form Submission</h3>
+
+        <p><strong>Name:</strong> {$name}</p>
+
+        <p><strong>Email:</strong> {$email}</p>
+
+        <p><strong>Message:</strong></p>
+
+        <p>{$message}</p>
+        ";
+
+        $mail->send();
+
+        echo "OK";
+
+    } catch (Exception $e) {
+
+        http_response_code(500);
+
+        echo "Error";
+    }
+}
 ?>
